0915f6c4269f14fd67eff772462b0bcb34e4569f
Dean Moses <moses@tacocat.com>
2023-10-23 18:31:30 -0400

    Disabled jest testing every file in the system every time I saved any file

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -2,14 +2,17 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
+import type { Config } from 'jest';
 
-export default {
+const config: Config = {
     transform: {
         '^.+\\.ts?$': 'ts-jest',
     },
     clearMocks: true,
-    collectCoverage: true,
+    collectCoverage: false,
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     testMatch: ['**/*.spec.ts'],
 };
+
+export default config;
