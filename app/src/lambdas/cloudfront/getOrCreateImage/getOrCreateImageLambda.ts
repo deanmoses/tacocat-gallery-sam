78737c008b62638797ab796ad196dab07954df7d
Dean Moses <moses@tacocat.com>
2023-10-29 16:11:19 -0700

    Deploying Sharp in a Lambda @ Edge, but not doing anything with it yet

--- app/src/lambdas/cloudfront/getOrCreateImage/getOrCreateImageLambda.ts
+++ app/src/lambdas/cloudfront/getOrCreateImage/getOrCreateImageLambda.ts
@@ -1,5 +1,6 @@
 import { CloudFrontResponseEvent, CloudFrontResponseResult, Handler } from 'aws-lambda';
 import { getOrCreateImage } from './getOrCreateImage';
+import sharp from 'sharp';
 
 /**
  * A Lambda used by CloudFront to resize images
@@ -8,6 +9,8 @@ export const handler: Handler = async (event: CloudFrontResponseEvent): Promise<
     console.info('I got this event: ', event);
     console.info('CloudFormation record: ', event.Records[0].cf);
     await getOrCreateImage();
+
+    console.log(sharp);
     event.Records[0].cf.response.headers['moses_header'] = [{ key: 'moses_header', value: 'moses_header_value' }];
     console.info('getOrCreateImageLambda(): DONE!');
 
