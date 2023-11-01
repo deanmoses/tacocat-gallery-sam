c3a9d1979f02a3c5a17cf0a5e04c7aac8862e7c9
Dean Moses <moses@tacocat.com>
2023-11-01 08:38:31 -0700

    Changed logging and comments

--- app/src/lambdas/processImageUpload/processImageUploadLambda.ts
+++ app/src/lambdas/processImageUpload/processImageUploadLambda.ts
@@ -3,20 +3,22 @@ import { extractImageMetadata } from './extractImageMetadata';
 import { createImage } from '../../lib/gallery/createImage/createImage';
 
 /**
- * A Lambda that extracts metadata from an image stored in S3.
+ * A Lambda that processes an image uploaded to S3.
+ * Extracts metadata from image and saves entry to DynamoDB.
  */
 export const handler: Handler = async (event: S3Event, context: Context, callback) => {
     const record = event.Records[0];
-    //console.info('I got this event: ', event);
+    //console.info('event: ', event);
 
     if (!record.eventName.includes('Removed') && !record.eventName.includes('Created')) {
         console.info(`Unhandled event: [${record.eventName}]`);
         callback(`Unhandled event: [${record.eventName}]`);
     }
 
-    console.info('processImageUpload - object key: ', record.s3.object.key);
+    console.info(`processImageUpload - object key: [${record.s3.object.key}]`);
     const imageMetadata = await extractImageMetadata(record.s3.bucket.name, record.s3.object.key);
     const imagePath = '/' + record.s3.object.key;
+    // Create image in DynamoDB
     await createImage(imagePath, imageMetadata);
 
     console.info('DONE!');
