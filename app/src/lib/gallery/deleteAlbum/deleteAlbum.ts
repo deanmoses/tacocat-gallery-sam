211951251b5f4cfc229bf274d87eb27b074ec29f
Dean Moses <moses@tacocat.com>
2023-11-02 09:22:42 -0700

    Delete Album API: blocks delete if album contains sub-albums

--- app/src/lib/gallery/deleteAlbum/deleteAlbum.ts
+++ app/src/lib/gallery/deleteAlbum/deleteAlbum.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, ExecuteStatementCommand } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, DeleteCommand } from '@aws-sdk/lib-dynamodb';
 import { getParentAndNameFromPath } from '../../gallery_path_utils/getParentAndNameFromPath';
 import { BadRequestException } from '../../lambda_utils/BadRequestException';
@@ -19,18 +19,50 @@ export async function deleteAlbum(albumPath: string) {
         throw new BadRequestException('Malformed album path: cannot delete root album');
     }
 
+    if (await albumContainsChildAlbums(albumPath)) {
+        throw new BadRequestException(`Album [${albumPath}] contains child albums, and thus cannot be deleted.`);
+    }
+
+    // TODO: delete child images
+
+    deleteAlbumFromDynamoDB(albumPath);
+}
+
+/**
+ * Return whether or not the specified album contains sub-albums
+ *
+ * @param albumPath Path of album, like /2001/12-31/
+ */
+async function albumContainsChildAlbums(albumPath: string): Promise<boolean> {
+    const ddbCommand = new ExecuteStatementCommand({
+        Statement:
+            'SELECT itemName' +
+            ` FROM "${getDynamoDbTableName()}"` +
+            ` WHERE parentPath='${albumPath}' AND itemType='album'`,
+    });
     const ddbClient = new DynamoDBClient({});
     const docClient = DynamoDBDocumentClient.from(ddbClient);
+    const results = await docClient.send(ddbCommand);
+    return !!results?.Items?.length;
+}
 
-    // TODO: block delete if the album contains child photos or child albums
-    const tableName = getDynamoDbTableName();
+/**
+ * Delete the album from DynamoDB.  Does NOT delete any child albums.
+ * TODO: add some sort of expression to prevent the delete from happening if there are child albums
+ *
+ * @param albumPath Path of album, like /2001/12-31/
+ */
+async function deleteAlbumFromDynamoDB(albumPath: string) {
     const pathParts = getParentAndNameFromPath(albumPath);
     const ddbCommand = new DeleteCommand({
-        TableName: tableName,
+        TableName: getDynamoDbTableName(),
         Key: {
             parentPath: pathParts.parent,
             itemName: pathParts.name,
         },
     });
+
+    const ddbClient = new DynamoDBClient({});
+    const docClient = DynamoDBDocumentClient.from(ddbClient);
     await docClient.send(ddbCommand);
 }
