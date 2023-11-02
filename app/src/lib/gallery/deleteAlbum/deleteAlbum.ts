7f781cfbb06a56b73cd5a0df8de422646fbb6d50
Dean Moses <moses@tacocat.com>
2023-11-02 09:28:39 -0700

    Delete Album: enforces album cannot be deleted if it contains images

--- app/src/lib/gallery/deleteAlbum/deleteAlbum.ts
+++ app/src/lib/gallery/deleteAlbum/deleteAlbum.ts
@@ -19,26 +19,23 @@ export async function deleteAlbum(albumPath: string) {
         throw new BadRequestException('Malformed album path: cannot delete root album');
     }
 
-    if (await albumContainsChildAlbums(albumPath)) {
-        throw new BadRequestException(`Album [${albumPath}] contains child albums, and thus cannot be deleted.`);
+    if (await albumContainsChildren(albumPath)) {
+        throw new BadRequestException(
+            `Album [${albumPath}] contains child photos or child albums, and thus cannot be deleted.`,
+        );
     }
-
-    // TODO: delete child images
 
     deleteAlbumFromDynamoDB(albumPath);
 }
 
 /**
- * Return whether or not the specified album contains sub-albums
+ * Return whether or not the specified album contains photos or child albums
  *
  * @param albumPath Path of album, like /2001/12-31/
  */
-async function albumContainsChildAlbums(albumPath: string): Promise<boolean> {
+async function albumContainsChildren(albumPath: string): Promise<boolean> {
     const ddbCommand = new ExecuteStatementCommand({
-        Statement:
-            'SELECT itemName' +
-            ` FROM "${getDynamoDbTableName()}"` +
-            ` WHERE parentPath='${albumPath}' AND itemType='album'`,
+        Statement: `SELECT itemName FROM "${getDynamoDbTableName()}" WHERE parentPath='${albumPath}'`,
     });
     const ddbClient = new DynamoDBClient({});
     const docClient = DynamoDBDocumentClient.from(ddbClient);
