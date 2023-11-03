5db37bef2bcbfc37381abe9a849da461eefa112b
Dean Moses <moses@tacocat.com>
2023-11-03 09:53:11 -0700

    Image processor no longer receives the unwanted S3 ObjectCopy event

--- app/src/lambdas/processImageUpload/processImageUploadLambda.ts
+++ app/src/lambdas/processImageUpload/processImageUploadLambda.ts
@@ -9,26 +9,36 @@ import { setImageAsParentAlbumThumbnailIfNoneExists } from '../../lib/gallery/se
  */
 export const handler: Handler = async (event: S3Event, context: Context, callback) => {
     const record = event.Records[0];
-    //console.info('event: ', event);
 
-    if (!record.eventName.includes('Removed') && !record.eventName.includes('Created')) {
-        console.info(`Unhandled event: [${record.eventName}]`);
-        callback(`Unhandled event: [${record.eventName}]`);
+    console.info(`Image processor: got event [${record?.eventName}]`);
+
+    // Handle all ObjectCreated events EXCEPT for Copy
+    if (
+        !record?.eventName ||
+        !record.eventName.includes('ObjectCreated') ||
+        record.eventName.includes('ObjectCreated:Copy')
+    ) {
+        console.warn(
+            `Image processor: got unexpected event [${record?.eventName}]. There's probably a misconfiguration.`,
+        );
+        if (!!callback) {
+            callback(`Unhandled event: [${record?.eventName}]`);
+        }
     }
 
-    console.info(`processImageUpload - object key: [${record.s3.object.key}]`);
+    console.info(`Image Processor: processing object key [${record.s3.object.key}]`);
 
     // Extract EXIF / IPTC metadata from image
     const imageMetadata = await extractImageMetadata(record.s3.bucket.name, record.s3.object.key);
 
     // Create image in DynamoDB
     const imagePath = '/' + record.s3.object.key;
-    console.info(`Creating image [${imagePath}] in DynamoDB`);
+    console.info(`Image Processor: creating image [${imagePath}] in DynamoDB`);
     await createImage(imagePath, imageMetadata);
 
     // Set image as its album's thumbnail, if the album does not already have a thumbnail
-    console.info(`Setting image [${imagePath}] as thumbnail of parent album if none exists`);
+    console.info(`Image Processor: setting image [${imagePath}] as thumbnail of parent album if none exists`);
     await setImageAsParentAlbumThumbnailIfNoneExists(imagePath);
 
-    console.info('DONE!');
+    console.info('Image Processor: DONE');
 };
