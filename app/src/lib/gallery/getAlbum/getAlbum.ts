4677c9ea0b21a550f18d28e607bd2c3710d2af5e
Dean Moses <moses@tacocat.com>
2023-11-05 15:59:57 -0500

    getAlbum() now has a strongly typed return

--- app/src/lib/gallery/getAlbum/getAlbum.ts
+++ app/src/lib/gallery/getAlbum/getAlbum.ts
@@ -4,13 +4,14 @@ import { getParentAndNameFromPath } from '../../gallery_path_utils/getParentAndN
 import { isValidAlbumPath } from '../../gallery_path_utils/pathValidator';
 import { BadRequestException } from '../../lambda_utils/BadRequestException';
 import { getDynamoDbTableName } from '../../lambda_utils/Env';
+import { Album } from '../galleryTypes';
 
 /**
  * Retrieve an album from DynamoDB.  Does not retrieve any child photos or child albums.
  *
  * @param albumPath Path of the album to retrieve, like /2001/12-31/
  */
-export async function getAlbum(albumPath: string) {
+export async function getAlbum(albumPath: string): Promise<Album | undefined> {
     if (!isValidAlbumPath(albumPath)) {
         throw new BadRequestException(`Malformed album path: [${albumPath}]`);
     }
