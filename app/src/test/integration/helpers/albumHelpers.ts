49da535fbbc7c1d46f76a5ff9f6e75c02c2155ce
Dean Moses <moses@tacocat.com>
2023-11-06 01:07:57 -0500

    Added function to clean up not only an album but also its parent album

--- app/src/test/integration/helpers/albumHelpers.ts
+++ app/src/test/integration/helpers/albumHelpers.ts
@@ -2,11 +2,26 @@ import { deleteAlbum } from '../../../lib/gallery/deleteAlbum/deleteAlbum';
 import { deleteImage } from '../../../lib/gallery/deleteImage/deleteImage';
 import { getAlbumAndChildren } from '../../../lib/gallery/getAlbum/getAlbumAndChildren';
 import { itemExists } from '../../../lib/gallery/itemExists/itemExists';
+import { getParentFromPath } from '../../../lib/gallery_path_utils/getParentFromPath';
 
 /**
- * Delete album and its children from S3 and DynamoDB.
+ * Delete album, its images AND its parent album from S3 and DynamoDB.
  * For cleaning up after tests.
- * @param albumPath
+ *
+ * @param albumPath path of album, like /2001/12-31/
+ */
+export async function cleanUpAlbumAndParents(albumPath: string): Promise<void> {
+    await cleanUpAlbum(albumPath);
+    const parentAlbumPath = getParentFromPath(albumPath);
+    await cleanUpAlbum(parentAlbumPath);
+}
+
+/**
+ * Delete album and its images from S3 and DynamoDB.
+ * Does not delete parent album.
+ * For cleaning up after tests.
+ *
+ * @param albumPath path of album, like /2001/12-31/
  */
 export async function cleanUpAlbum(albumPath: string): Promise<void> {
     const children = (await getAlbumAndChildren(albumPath))?.children;
