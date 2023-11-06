0f7391002670caaf8842d05b89cd02dceadcabac
Dean Moses <moses@tacocat.com>
2023-11-05 22:44:59 -0500

    Added documentation

--- app/src/lib/gallery_client/AlbumObject.ts
+++ app/src/lib/gallery_client/AlbumObject.ts
@@ -1,5 +1,11 @@
 import { Album, AlbumResponse } from '../gallery/galleryTypes';
 
+/**
+ * Find image in album by the image's name
+ *
+ * @param album album object
+ * @param imagePath name of image, like image.jpg
+ */
 export function findImage(album: AlbumResponse, imageName: string): Album | undefined {
     return album?.children?.find((child) => child.itemName === imageName);
 }
