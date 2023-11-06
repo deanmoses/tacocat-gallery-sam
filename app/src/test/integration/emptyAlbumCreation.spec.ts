58a019b5d5d14d1c1dcb59435f7889d39c5d6037
Dean Moses <moses@tacocat.com>
2023-11-06 02:02:36 -0500

    Added more assertions, setting year to the 1700's
    Setting integration test year albums to the 1700's to easily identify poor cleanup of data in the database

--- app/src/test/integration/emptyAlbumCreation.spec.ts
+++ app/src/test/integration/emptyAlbumCreation.spec.ts
@@ -5,10 +5,10 @@ import { itemExists } from '../../lib/gallery/itemExists/itemExists';
 import { getParentAndNameFromPath } from '../../lib/gallery_path_utils/getParentAndNameFromPath';
 import { assertDynamoDBItemDoesNotExist, cleanUpAlbum } from './helpers/albumHelpers';
 
-const albumPath = '/1962/08-13/'; // unique to this suite to prevent pollution
+const albumPath = '/1701/08-13/'; // unique to this suite to prevent pollution
 
 beforeAll(async () => {
-    assertDynamoDBItemDoesNotExist(albumPath);
+    await assertDynamoDBItemDoesNotExist(albumPath);
 });
 
 afterAll(async () => {
@@ -28,20 +28,18 @@ it('fails if album already exists', async () => {
     await expect(createAlbum(albumPath)).rejects.toThrow(/exists/);
 });
 
-test('doesnt throw if album already exists', async () => {
-    await expect(createAlbum(albumPath, false /* don't error if it exists */)).resolves.not.toThrow();
-});
-
-test('getAlbum()', async () => {
+test('get empty album', async () => {
     const album = await getAlbumAndChildren(albumPath);
     if (!album) throw new Error(`No album`);
     if (album.children && album.children.length > 0) throw new Error(`Album has children, that's not expected`);
     const albumPathParts = getParentAndNameFromPath(albumPath);
     expect(album.album?.itemName).toBe(albumPathParts.name);
     expect(album.album?.parentPath).toBe(albumPathParts.parent);
+    expect(album.album?.title).toBeUndefined();
+    expect(album.album?.description).toBeUndefined();
+    expect(album.album?.published).toBeUndefined();
+    expect(album.album?.thumbnail?.path).toBeUndefined();
 });
-
-test.todo('getAlbum() has no thumb');
 
 test('deleteAlbum() succeeds on empty album', async () => {
     await expect(deleteAlbum(albumPath)).resolves.not.toThrow();
