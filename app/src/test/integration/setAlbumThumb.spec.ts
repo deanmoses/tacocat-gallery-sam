e50fbdaef96e5905508dc61c4147c417a4d51699
Dean Moses <moses@tacocat.com>
2023-11-05 19:03:39 -0500

    Added tests for setting thumbnail on grandparent album

--- app/src/test/integration/setAlbumThumb.spec.ts
+++ app/src/test/integration/setAlbumThumb.spec.ts
@@ -1,11 +1,10 @@
-import { createAlbum } from '../../lib/gallery/createAlbum/createAlbum';
 import { deleteImage } from '../../lib/gallery/deleteImage/deleteImage';
 import { getAlbum } from '../../lib/gallery/getAlbum/getAlbum';
-import { itemExists } from '../../lib/gallery/itemExists/itemExists';
 import { setAlbumThumbnail } from '../../lib/gallery/setAlbumThumbnail/setAlbumThumbnail';
+import { getParentFromPath } from '../../lib/gallery_path_utils/getParentFromPath';
 import { isValidAlbumPath, isValidImagePath } from '../../lib/gallery_path_utils/pathValidator';
-import { cleanUpAlbum } from './helpers/albumHelpers';
-import { imageExistsInOriginalsBucket, uploadImage } from './helpers/s3ImageHelper';
+import { assertItemExists, cleanUpAlbum } from './helpers/albumHelpers';
+import { assertImageExistsInOriginalsBucket, uploadImage } from './helpers/s3ImageHelper';
 
 const albumPath = '/1949/10-04/'; // unique to this suite to prevent pollution
 const imagePath1 = `${albumPath}image1.jpg`;
@@ -16,20 +15,18 @@ beforeAll(async () => {
     expect(isValidImagePath(imagePath1)).toBe(true);
     expect(isValidImagePath(imagePath2)).toBe(true);
 
-    if (await itemExists(albumPath)) throw new Error(`Album [${albumPath}] cannot exist at start of this test suite`);
-
-    await createAlbum(albumPath);
     await uploadImage('image.jpg', imagePath1);
     await uploadImage('image.jpg', imagePath2);
 
     await new Promise((r) => setTimeout(r, 4000)); // wait for image processing lambda to be triggered
 
-    await expect(itemExists(albumPath)).resolves.toBe(true);
-    await expect(itemExists(imagePath1)).resolves.toBe(true);
-    await expect(itemExists(imagePath2)).resolves.toBe(true);
+    await assertItemExists(albumPath);
+    await assertItemExists(getParentFromPath(albumPath));
+    await assertItemExists(imagePath1);
+    await assertItemExists(imagePath2);
 
-    await expect(imageExistsInOriginalsBucket(imagePath1)).resolves.toBe(true);
-    await expect(imageExistsInOriginalsBucket(imagePath2)).resolves.toBe(true);
+    await assertImageExistsInOriginalsBucket(imagePath1);
+    await assertImageExistsInOriginalsBucket(imagePath2);
 }, 10000 /* increase Jest's timeout */);
 
 afterAll(async () => {
@@ -46,8 +43,22 @@ test('Set Thumbnail - happy path', async () => {
     expect(album?.thumbnail?.path).toBe(imagePath2);
 });
 
+test('Set Thumbnail on grandparent', async () => {
+    const grandparentPath = getParentFromPath(albumPath);
+    await expect(setAlbumThumbnail(grandparentPath, imagePath2)).resolves.not.toThrow();
+    const album = await getAlbum(grandparentPath);
+    expect(album?.thumbnail?.path).toBe(imagePath2);
+});
+
 test('Delete Image Should Unset Thumbnail', async () => {
     await expect(deleteImage(imagePath2)).resolves.not.toThrow();
+
+    // Shouldn't be thumbnail of parent
     const album = await getAlbum(albumPath);
     expect(album?.thumbnail).toBeUndefined();
+
+    // Shouldn't be thumbnail of grandparent
+    const grandparentPath = getParentFromPath(albumPath);
+    const grandparentAlbum = await getAlbum(grandparentPath);
+    expect(grandparentAlbum?.thumbnail).toBeUndefined();
 });
