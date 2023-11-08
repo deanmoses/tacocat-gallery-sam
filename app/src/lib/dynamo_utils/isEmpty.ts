57f9ee47a297c8d3bad642194c2ec43ad1034dac
Dean Moses <moses@tacocat.com>
2023-11-08 01:14:36 -0500

    Unused

--- app/src/lib/dynamo_utils/isEmpty.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @param {String} s
- * @returns true if string is one of the empty values that DynamoDB can't save,
- * like is undefined, null or empty: any
- */
-export function isEmpty(s: string | boolean | null | undefined): boolean {
-    return s === undefined || s === null || s === '';
-}
