ff50c235fe9625025f462ab5f76a8cffcbcfdb26
Dean Moses <moses@tacocat.com>
2023-11-09 09:22:58 -0500

    Refactoring
    No functional changes
    No changes that bleed external to the file

--- app/src/lib/gallery_path_utils/pathValidator.ts
+++ app/src/lib/gallery_path_utils/pathValidator.ts
@@ -1,5 +1,6 @@
 /**
- * Determine if specified string is a valid album or image path such as / or /2001/ or /2001/12-31/ or /2001/12-31/image.jpg
+ * Determine if specified string is a valid album or image path
+ * such as / or /2001/ or /2001/12-31/ or /2001/12-31/image.jpg
  */
 export function isValidPath(path: string): boolean {
     return isValidAlbumPath(path) || isValidImagePath(path);
@@ -10,21 +11,7 @@ export function isValidPath(path: string): boolean {
  * Must be root, year or album path like / or /2001/ or /2001/12-31/
  */
 export function isValidAlbumPath(path: string): boolean {
-    return getAlbumPathRegex().test(path);
-}
-
-/**
- * Root, year or album path like / or /2001/ or /2001/12-31/
- */
-function getAlbumPathRegex() {
-    return new RegExp(getAlbumPathRegexString());
-}
-
-/**
- * Root, year or album path like / or /2001/ or /2001/12-31/
- */
-function getAlbumPathRegexString() {
-    return /^(\/\d\d\d\d(\/(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))?)?\/$/;
+    return /^(\/\d\d\d\d(\/(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))?)?\/$/.test(path);
 }
 
 /**
@@ -48,14 +35,7 @@ export function isValidDayAlbumName(dayAlbumName: string): boolean {
  * Must be an image on a day album like /2001/12-31/image.jpg
  */
 export function isValidImagePath(path: string): boolean {
-    return getImagePathRegex().test(path);
-}
-
-function getImagePathRegex() {
-    return new RegExp(getImagePathRegexString());
-}
-function getImagePathRegexString() {
-    return /^\/\d\d\d\d\/(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\/[a-zA-Z0-9_-]+\.(jpg|jpeg|gif|png)$/i;
+    return /^\/\d\d\d\d\/(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])\/[a-zA-Z0-9_-]+\.(jpg|jpeg|gif|png)$/i.test(path);
 }
 
 /**
@@ -65,11 +45,7 @@ function getImagePathRegexString() {
  * @param imageName image name like 'image.jpg'
  */
 export function isValidImageName(imageName: string): boolean {
-    return new RegExp(getImageNameRegex()).test(imageName);
-}
-
-function getImageNameRegex() {
-    return /^[a-zA-Z0-9_-]+\.(jpg|jpeg|gif|png)$/i;
+    return /^[a-zA-Z0-9_-]+\.(jpg|jpeg|gif|png)$/i.test(imageName);
 }
 
 /**
@@ -79,11 +55,7 @@ function getImageNameRegex() {
  * @param imageName image name like 'image.jpg'
  */
 export function isValidImageNameStrict(imageName: string): boolean {
-    return new RegExp(getImageNameStrictRegex()).test(imageName);
-}
-
-function getImageNameStrictRegex() {
-    return /^[a-z0-9]+([a-z0-9_]*[a-z0-9]+)*\.(jpg|gif|png)$/;
+    return /^[a-z0-9]+([a-z0-9_]*[a-z0-9]+)*\.(jpg|gif|png)$/.test(imageName);
 }
 
 /**
