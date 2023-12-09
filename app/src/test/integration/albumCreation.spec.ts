cf550a331847132ee02a369aa005f950b61dffc4
Dean Moses <moses@tacocat.com>
2023-12-09 13:41:34 -0800

    Fixed failing test
    Fixed failing test
    Added some new tests
    Parallelized test

--- app/src/test/integration/albumCreation.spec.ts
+++ app/src/test/integration/albumCreation.spec.ts
@@ -4,18 +4,17 @@ import { getAlbumAndChildren } from '../../lib/gallery/getAlbum/getAlbum';
 import { itemExists } from '../../lib/gallery/itemExists/itemExists';
 import { getParentAndNameFromPath } from '../../lib/gallery_path_utils/galleryPathUtils';
 import { assertDynamoDBItemDoesNotExist } from './helpers/albumHelpers';
+import { updateAlbum } from '../../lib/gallery/updateAlbum/updateAlbum';
 
 const albumPath = '/1701/08-13/'; // unique to this suite to prevent pollution
 const albumPath2 = '/1701/08-14/';
 
 beforeAll(async () => {
-    await assertDynamoDBItemDoesNotExist(albumPath);
-    await assertDynamoDBItemDoesNotExist(albumPath2);
+    await Promise.all([assertDynamoDBItemDoesNotExist(albumPath), assertDynamoDBItemDoesNotExist(albumPath2)]);
 });
 
 afterAll(async () => {
-    await deleteAlbum(albumPath);
-    await deleteAlbum(albumPath2);
+    await Promise.allSettled([deleteAlbum(albumPath), deleteAlbum(albumPath2)]);
 });
 
 it('should fail on invalid album path', async () => {
@@ -27,8 +26,13 @@ test('album creation should succeed', async () => {
     await expect(itemExists(albumPath)).resolves.toBe(true);
 });
 
-test('getAlbum retrieves newly created album', async () => {
+test('retrieve unpublished album should fail', async () => {
     const album = await getAlbumAndChildren(albumPath);
+    if (album) throw new Error(`Should not be able to retrieve unpublished album`);
+});
+
+test('retrieve unpublished album should succed', async () => {
+    const album = await getAlbumAndChildren(albumPath, true /* get unpublished albums */);
     if (!album) throw new Error(`No album`);
     if (album.children && album.children.length > 0) throw new Error(`Album has children, that's not expected`);
     const albumPathParts = getParentAndNameFromPath(albumPath);
@@ -40,7 +44,24 @@ test('getAlbum retrieves newly created album', async () => {
     expect(album?.thumbnail?.path).toBeUndefined();
 });
 
-it('should fail to create if album already exists', async () => {
+test('publishing album should succeed', async () => {
+    await expect(updateAlbum(albumPath, { published: true })).resolves.not.toThrow();
+});
+
+test('retrieving published album should succeed', async () => {
+    const album = await getAlbumAndChildren(albumPath, false /* don't get unpublished albums */);
+    if (!album) throw new Error(`No album`);
+    if (album.children && album.children.length > 0) throw new Error(`Album has children, that's not expected`);
+    const albumPathParts = getParentAndNameFromPath(albumPath);
+    expect(album?.itemName).toBe(albumPathParts.name);
+    expect(album?.parentPath).toBe(albumPathParts.parent);
+    expect(album?.path).toBe(albumPath);
+    expect(album?.description).toBeUndefined();
+    expect(album?.published).toBeTruthy();
+    expect(album?.thumbnail?.path).toBeUndefined();
+});
+
+it('should fail to create album that already exists', async () => {
     await expect(createAlbum(albumPath)).rejects.toThrow(/exists/i);
 });
 
@@ -48,13 +69,12 @@ it('should fail on unknown attribute', async () => {
     await expect(createAlbum(albumPath, { unknownAttr: '' })).rejects.toThrow(/unknown/i);
 });
 
-test('deleteAlbum() succeeds on empty album', async () => {
+test('delete succeeds on empty album', async () => {
     await expect(deleteAlbum(albumPath)).resolves.not.toThrow();
 });
 
 it('create with attributes', async () => {
     await createAlbum(albumPath2, {
-        title: 'Title 1',
         description: 'Description 1',
         summary: 'Summary 1',
         published: true,
@@ -62,7 +82,7 @@ it('create with attributes', async () => {
     await expect(itemExists(albumPath2)).resolves.toBe(true);
 });
 
-test('getAlbum retrieves updated album', async () => {
+test('retrieving album created with attributes to succeed', async () => {
     const album = await getAlbumAndChildren(albumPath2);
     if (!album) throw new Error(`No album`);
     if (album.children && album.children.length > 0) throw new Error(`Album has children, that's not expected`);
